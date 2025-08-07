a7d572a8b1ba135ebe24de516025fe2d4a7369c1
LiuKeying <ke_ying_liu@163.com>
2025-08-07 20:06:56 +0800

    title & icon

---
+++ src/views/users.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="manage-page">
+        <div class="detail">
+            <div class="detail-page">
+                <div class="loading-container">
+                    <div class="empty-state">
+                        <Icon type="ios-information-circle" size="60" color="#c5c8ce" />
+                        <p>功能未完成</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
